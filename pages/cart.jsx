7781da4b2cc3fdf3980d3dac37b9f40af1e1c130
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2022-08-09 18:10:26 +0100

    login form validation

---
+++ pages/cart.jsx
@@ -0,0 +1,140 @@
+/* eslint-disable @next/next/no-img-element */
+import React, { useState, useEffect } from "react";
+import { useContext } from "react";
+import { Store } from "../utils/Store";
+import Link from "next/link";
+import Head from "next/head";
+import Navbar from "../components/navbar";
+import Image from "next/image";
+import { XCircleIcon } from "@heroicons/react/outline";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+
+const CartScreen = () => {
+  const router = useRouter();
+  const { state, dispatch } = useContext(Store);
+
+  const handleDeleteFromCart = (product) => {
+    dispatch({ type: "CART_REMOVE_ITEM", payload: product });
+  };
+
+  const handleQtyChange = (item, countInStock) => {
+    const quantity = +countInStock;
+
+    dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
+  };
+
+  return (
+    <div>
+      <Head>
+        <title>Cart</title>
+        <meta name='description' content='Zicomm' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='flex min-h-screen justify-between flex-col'>
+        <header>
+          <Navbar />
+        </header>
+        <main className='container m-auto mt-4 px-4 '>
+          <h1 className='mb-4 text-xl'>Shopping Cart</h1>
+          {state.cart.cartItems.length === 0 ? (
+            <div>
+              Cart is Empty. <Link href='/'>Go Shopping</Link>
+            </div>
+          ) : (
+            <div className='grid md:grid-cols-4 md:gap-5'>
+              <div className='overflow-x-auto md:col-span-3'>
+                <table className='min-w-full'>
+                  <thead className='border-b'>
+                    <tr>
+                      <th className='px-5 text-left'>Item</th>
+                      <th className='p-5 text-right'>Quantity</th>
+                      <th className='p-5 text-right'>Price</th>
+                      <th className='p-5 '>Action</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {state.cart.cartItems.map((item) => (
+                      <tr className='border-b ' key={item.slug}>
+                        <td>
+                          <Link href={`/product/${item.slug}`}>
+                            <a className='flex items-center  text-center'>
+                              <Image
+                                src={item.image}
+                                alt={item.name}
+                                width={50}
+                                height={50}
+                              />
+                              &nbsp;
+                              {item.name}
+                            </a>
+                          </Link>
+                        </td>
+                        <td className='p-5 text-right'>
+                          <select
+                            value={item.quantity}
+                            onChange={(e) =>
+                              handleQtyChange(item, e.target.value)
+                            }
+                          >
+                            {[...Array(item.countInStock).keys()].map(
+                              (item) => (
+                                <option value={item + 1} key={item + 1}>
+                                  {item + 1}
+                                </option>
+                              )
+                            )}
+                          </select>
+                        </td>
+                        <td className='p-5 text-right'>{item.price}</td>
+                        <td className='p-5 text-center'>
+                          <button onClick={() => handleDeleteFromCart(item)}>
+                            <XCircleIcon className='h-5 w-5' />
+                          </button>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              <div>
+                <div className='card p-5'>
+                  <ul>
+                    <li>
+                      <div className='pb-3 text-lg font-bold'>
+                        Subtotal (
+                        {state.cart.cartItems.reduce(
+                          (acc, cur) => acc + cur.quantity,
+                          0
+                        )}
+                        ) : &nbsp; $
+                        {state.cart.cartItems.reduce(
+                          (acc, cur) => acc + cur.price * cur.quantity,
+                          0
+                        )}
+                      </div>
+                    </li>
+                    <li>
+                      <button
+                        onClick={() => router.push("login?redirect=/shipping")}
+                        className='primary-button w-full '
+                      >
+                        Checkout
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          )}
+        </main>
+        <footer className='flex justify-center items-center h-10 shadow-inner'>
+          Copyright &copy; 2022 Zicomm
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false });
---
+++ pages/login.jsx
@@ -0,0 +1,89 @@
+import Head from "next/head";
+import Link from "next/link";
+import React from "react";
+import Navbar from "../components/navbar";
+import { useForm } from "react-hook-form";
+
+const LoginScreen = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const formHandler = ({ email, password }) => {};
+
+  return (
+    <div>
+      <Head>
+        <title>Cart</title>
+        <meta name='description' content='Zicomm' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <div className='flex min-h-screen justify-between flex-col'>
+        <header>
+          <Navbar />
+        </header>
+        <main className='container m-auto mt-4 px-4 '>
+          <form
+            className='mx-auto max-w-screen-md'
+            onSubmit={handleSubmit(formHandler)}
+          >
+            <h1 className='mb-4 text-xl '>Login</h1>
+            <div className='mb-4'>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                id='email'
+                className='w-full'
+                autoFocus
+                {...register("email", {
+                  required: "Please enter email address",
+                  pattern: {
+                    value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                    message: "Please enter valid email",
+                  },
+                })}
+              />
+              {errors.email && (
+                <p className='text-red-500'>{errors.email.message}</p>
+              )}
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                id='password'
+                className='w-full'
+                autoFocus
+                {...register("password", {
+                  required: "Please enter your password",
+                  minLength: {
+                    value: 8,
+                    message: "password should be more than 8 chars",
+                  },
+                })}
+              />
+              {errors.password && (
+                <p className='text-red-500'>{errors.password.message}</p>
+              )}
+            </div>
+            <div className='mb-4'>
+              <button className='primary-button'>Login</button>
+            </div>
+            <div className='mb-4'>
+              Dont&apos;t have an account? &nbsp;
+              <Link href='/register'>Register</Link>
+            </div>
+          </form>
+        </main>
+        <footer className='flex justify-center items-center h-10 shadow-inner'>
+          Copyright &copy; 2022 Zicomm
+        </footer>
+      </div>
+    </div>
+  );
+};
+
+export default LoginScreen;
